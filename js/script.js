76d6456bc3021f64307d53f58bd2345a7e0ac04c
Nick Davies <nickdavies100@gmail.com>
2020-05-12 14:41:50 -0700

    Latest Update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 
 //Set focus on first input field
-document.getElementById("name").focus();
+document.getElementById('name').focus();
+//First hide the Div containing other job label and text input
+document.getElementById('other-job').style.display = 'none';
+//Disable Color dropdown and set initial text
 
-const x = document.getElementById("jobRole").value;
+const colorPick = document.getElementById('color');
+colorPick.disabled = true;
+colorPick.add("Pick Your Theme First");
 
-  // Get the checkbox
+//Tshirt Color Function
+var colorDefault = document.getElementById('design');
+colorDefault.size = 0;
 
-  // If the checkbox is checked, display the output text
-if (x == '5'){
-    var input = document. createElement("input"); input. setAttribute('type', 'text');
-    var foo = document.getElementById("title");
-    foo.appendChild(element);
+var e = document.getElementById("title");
+e.addEventListener("change", titleTest);
+//Event listener for change to the job role. Then sends to function titleTest
 
-  } else {
-
+function titleTest(){
+  let jobTitle = e.options[e.selectedIndex].text;
+  //If the jobTitle is other it will show the hidden div.
+  if (jobTitle === 'Other'){
+    document.getElementById('other-job').style.display = 'block';
   }
+};
+
+
+colorDefault.addEventListener("change", showColors)
+
+function showColors(){
+  colorPick.remove("Pick Your Theme First");
+  document.getElementById('color').disabled = false;
+}
