d49f7d3abac97d4d86819dcd4371ce231d4faf08
Nick Davies <nickdavies100@gmail.com>
2020-05-12 11:25:23 -0700

    update
    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('test');
+
+//Set focus on first input field
+document.getElementById("name").focus();
+
+const x = document.getElementById("jobRole").value;
+
+  // Get the checkbox
+
+  // If the checkbox is checked, display the output text
+if (x == '5'){
+    var input = document. createElement("input"); input. setAttribute('type', 'text');
+    var foo = document.getElementById("title");
+    foo.appendChild(element);
+
+  } else {
+
+  }
