e3c07791f82df137392224438d3c75caa45bd142
Nick Davies <nickdavies100@gmail.com>
2020-05-13 09:48:55 -0700

    Functional Tshirt Section

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
+const colorPick = document.getElementById('color');
+const designPick = document.getElementById('design');
+
+//Function to remove all existing color options:
+//Found help here - https://stackoverflow.com/questions/3364493/how-do-i-clear-all-options-in-a-dropdown-box
+function clearColor(){
+  var clearColor = document.getElementById("color");
+  var colorLength = clearColor.options.length;
+  for (i = colorLength-1; i >= 0; i--) {
+    clearColor.options[i] = null;
+  }
+}
+
+clearColor();
+
+//Creates new Pick your Theme First option in color dropdown
+var option = document.createElement("option");
+option.text = "Pick Your Theme First";
+colorPick.add(option, colorPick[0]);
 
 //Set focus on first input field
 document.getElementById('name').focus();
 //First hide the Div containing other job label and text input
 document.getElementById('other-job').style.display = 'none';
 //Disable Color dropdown and set initial text
-
-const colorPick = document.getElementById('color');
-const colorPick2 = document.getElementById('color');
-
-const designPick = document.getElementById('design');
-
-//Removes all existing options in color pick section until theme is chosen
-for(i = 0; i=colorPick.length; i++){
-  colorPick.remove(colorPick.i);
-}
-var option = document.createElement("option");
-option.text = "Pick Your Theme First";
-colorPick.add(option);
 
 //Event listener for change to the job role. Then sends to function titleTest
 var e = document.getElementById("title");
@@ -31,24 +37,44 @@ function titleTest(){
   }
 };
 
-designPick.addEventListener("change", showColors)
-console.log(colorPick2);
+//Change listener to the selector options
+designPick.addEventListener("change", showColors);
 
 function showColors(){
-    colorPick.options[0] = null;
     const designTheme = document.getElementById("design").value;
     if(designTheme == 'js puns'){
-      colorPick.options[0] = null;
 
-      let newOption = new Option(colorPick2,'Option Value');
-      colorPick.add(newOption,undefined);
+      clearColor();
 
+      var newOption1 = document.createElement("option");
+      newOption1.text = ("Cornflower Blue (JS Puns shirt only");
+      colorPick.add(newOption1, colorPick[1]);
 
+      var newOption2 = document.createElement("option");
+      newOption2.text = ("Dark Slate Grey (JS Puns shirt only)");
+      colorPick.add(newOption2, colorPick[2]);
 
-
-
+      var newOption3 = document.createElement("option");
+      newOption3.text = ("Gold (JS Puns shirt only)");
+      colorPick.add(newOption3, colorPick[3]);
     }
+
     else if (designTheme == 'heart js'){
 
-    }
+      clearColor();
+
+      var newOption1 = document.createElement("option");
+      newOption1.text = ("Tomato (I " + `\u2764` + "JS shirt only)");
+      colorPick.add(newOption1, colorPick[1]);
+
+      var newOption2 = document.createElement("option");
+      newOption2.text = ("Steel Blue (I " + `\u2764` + "JS shirt only)");
+      colorPick.add(newOption2, colorPick[2]);
+
+      var newOption3 = document.createElement("option");
+      newOption3.text = ("Dim Grey (I " + `\u2764` + "JS shirt only)");
+      colorPick.add(newOption3, colorPick[3]);
+
+
+  }
 }
